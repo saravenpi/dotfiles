bbcba3072a881516f5a37a282144d701cbab6f08
Saravenpi Shigeturi <saravenpi@protonmail.com>
2021-10-27 03:14:06 +0200

    new aliases

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,17 @@
-alias g="git"
-alias nv="nvim"
+alias g git
+alias nv nvim
+alias c clear
+alias l ls
+alias q exit
+alias hc "history clear"
+alias gos "cd ~/code/.epita"
+alias goo "cd ~/code/dreamit"
+alias clp "gpaste-client"
+alias clpc "gpaste-client delete-history"
+function md
+    pandoc $argv | w3m -T text/html
+end
+
+# opam configuration
+source ~/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+set fish_greeting "Hey, Sara, what are we doing today ?"
