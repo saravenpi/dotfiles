f9e3e1593ebf9b5663dbf0261d8a1907179d34a7
Saravenpi Shigeturi <saravepi@tuta.io>
2024-03-05 11:39:35 +0100

    ✨ Better tmux navigation with vim, Telescope grep

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,10 +1,17 @@
 return {
-  "telescope.nvim",
-  dependencies = {
-    "nvim-telescope/telescope-fzf-native.nvim",
-    build = "make",
-    config = function()
-      require("telescope").load_extension("fzf")
-    end,
-  },
+    "telescope.nvim",
+    dependencies = {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        config = function()
+            require("telescope").load_extension("fzf")
+        end,
+        keys = {
+            {
+                "<leader>fg",
+                ":Telescope live_grep<cr>",
+                desc = "Find in files",
+            },
+        },
+    },
 }
---
+++ nvim/.config/nvim/lua/plugins/tmux.lua
@@ -0,0 +1,1 @@
+return { "alexghergh/nvim-tmux-navigation" }
