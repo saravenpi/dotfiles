e39146c373b03c19f227371df7fee6cf44e74873
Saravenpi <saravenpi@tuta.io>
2026-03-02 00:25:39 +0100

    ⬆️ upgrade nvim-colorizer.lua to 2025 config spec

--- nvim/.config/nvim/lua/config/plugins/colorizer.lua
+++ nvim/.config/nvim/lua/config/plugins/colorizer.lua
@@ -1,29 +1,63 @@
 local M = {}
 
 M.specs = {
-	{ src = "https://github.com/catgoose/nvim-colorizer.lua" },
+    { src = "https://github.com/catgoose/nvim-colorizer.lua" },
 }
 
 M.setup = function()
-	require("colorizer").setup({
-		filetypes = { "*" },
-		user_default_options = {
-			RGB = true,
-			RRGGBB = true,
-			names = false,
-			RRGGBBAA = true,
-			AARRGGBB = true,
-			rgb_fn = true,
-			hsl_fn = true,
-			css = true,
-			css_fn = true,
-			tailwind = true,
-			-- sass = { enable = true },
-			mode = "background",
-			virtualtext = "■",
-		},
-		buftypes = {},
-	})
+    require("colorizer").setup({
+        filetypes = { "*" },
+        buftypes = {},
+        user_commands = true,
+        lazy_load = false,
+        options = {
+            parsers = {
+                css = true,
+                css_fn = true,
+                names = {
+                    enable = false,
+                    lowercase = true,
+                    camelcase = true,
+                },
+                hex = {
+                    default = true,
+                },
+                rgb = { enable = true },
+                hsl = { enable = true },
+                oklch = { enable = false },
+                tailwind = {
+                    enable = true,
+                    lsp = false,
+                    update_names = false,
+                },
+                sass = {
+                    enable = true,
+                    parsers = { css = true },
+                },
+                xterm = { enable = true },
+            },
+            display = {
+                mode = "background",
+                background = {
+                    bright_fg = "#000000",
+                    dark_fg = "#ffffff",
+                },
+                virtualtext = {
+                    char = "■",
+                    position = "eol",
+                    hl_mode = "foreground",
+                },
+                priority = {
+                    default = 150,
+                    lsp = 200,
+                },
+            },
+            hooks = {
+                should_highlight_line = false,
+            },
+            always_update = false,
+        },
+    })
 end
 
 return M
